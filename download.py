062b2caaaf10548f8ffbbb22c0a86fbdfd5b4e51
dhayaenrique <61014659+dhayaenrique@users.noreply.github.com>
2020-02-14 17:04:10 +0100

    Add files via upload
    T4

---
+++ download.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+from pymatgen.ext.matproj import MPRester
+
+# Permet d'utiliser l'interface du MP
+with MPRester("2r9TvKUOgDsTx87F")as mpr:
+    
+    # Utilise la méthode "query" pour obtenir le fichier cif de CdBr2
+    data=mpr.query("mp-568690",["cif"])
+    
+
+
+# In[ ]:
+
+
+
+
